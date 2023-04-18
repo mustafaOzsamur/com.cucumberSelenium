8467c3cf6ae2c88ae657b111da5ebd0ea0252ff8
mustafaOzsamur <mustafaozsamur22@gmail.com>
2023-04-19 00:09:09 +0200

    Runner

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,42 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+     features = "src/test/resources/features" ,
+        glue = "stepdefinitions" ,
+        tags = "@wip",
+        dryRun = false
+)
+public class Runner {
+    /*
+    Cucumber'da Runner class'i bos bir class'tir
+    Runner class'ini diger class'lardan farkli kilan ve TestNG'deki
+    xml dosyalari gibi calismasini saglayan 2 adet notasyon mevcuttur.
+
+    @RunWith() notasyonu projemize cucumber Junit dependency eklememizin sebebidir
+    bu sayede runner class'larimiz cucumber ile calisir.
+
+    @CucumberOptions() notasyonu ile istedigimiz özellikleri Runner class'ina ekleyebiliriz
+
+    raporlama gibi ekstra optionlari da ekleyecegiz
+    Ancak
+    oncelikli görevi features dosyalari stepdefinitions da bulunan
+    Java methodlarini ilisiklendirmektir.
+
+    tags ="@wip" -----> features klasörü icerisinde yazilan tag(lari) aratip buldugu tum
+    features veya scenario'lari calistirir.
+
+    dryRun : iki deger alabilir
+    true : secilirse, verilen tag ile isaretli olan Feature veya Scenario'daki
+            eksik stepdefinitions'lari bulup ilgili methodlari olusturur
+            hic bir sekilde testimizi calistirmaz
+            eksik adim yoksa test passed olarak isaretler.
+
+     false : secilirse, verilen tag ile isaretlenen
+            Features veya Scenario'lari calistirir
+     */
+}
--- src/test/java/stepdefinitions/AmazonStepdefinitions.java
+++ src/test/java/stepdefinitions/AmazonStepdefinitions.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
@@ -17,6 +18,7 @@ public class AmazonStepdefinitions {
     }
     @Then("amazon arama kutusuna Nutella Yazip aratir")
     public void amazon_arama_kutusuna_nutella_yazip_aratir() {
+        amazonPage=new AmazonPage();
     amazonPage.amazonAramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
     }
     @Then("arama sonuclarinin Nutella icerdigini test eder")
@@ -32,4 +34,39 @@ public class AmazonStepdefinitions {
     Driver.closeDriver();
     }
 
+    @Then("amazon arama kutusuna Java Yazip aratir")
+    public void amazonAramaKutusunaJavaYazipAratir() {
+         amazonPage=new AmazonPage();
+        amazonPage.amazonAramaKutusu.sendKeys("Java"+ Keys.ENTER);
+    }
+
+    @And("arama sonuclarinin Java icerdigini test eder")
+    public void aramaSonuclarininJavaIcerdiginiTestEder() {
+        String actualAramaSonucu=amazonPage.aramaSonucuElementi.getText();
+        String expectedKelime="Java";
+
+        Assert.assertTrue(actualAramaSonucu.contains(expectedKelime));
+    }
+
+    @Then("amazon arama kutusuna Samsung Yazip aratir")
+    public void amazon_arama_kutusuna_samsung_yazip_aratir() {
+        amazonPage=new AmazonPage();
+        amazonPage.amazonAramaKutusu.sendKeys("Samsung"+ Keys.ENTER);
+    }
+    @Then("arama sonuclarinin Samsung icerdigini test eder")
+    public void arama_sonuclarinin_samsung_icerdigini_test_eder() {
+        String actualAramaSonucu=amazonPage.aramaSonucuElementi.getText();
+        String expectedKelime="Samsung";
+
+        Assert.assertTrue(actualAramaSonucu.contains(expectedKelime));
+    }
+
+    @And("{int} saniye bekler")
+    public void saniyeBekler(int saniye) {
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
 }
